7573945015ce73973736887712c43786250907cc
zeiliasgr <ze_iliasgr@yahoo.gr>
2013-07-12 00:27:39 +0000

    Latest addition

--- grader.js
+++ grader.js
@@ -24,7 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
+var URLFILE_DEFAULT = "https://raw.github.com/zeiliasgr/bitstarter/master/index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -65,10 +67,24 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_file>', 'URL to index.html', clone(assertFileExists), URLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+
+    if(program.url){
+	rest.get(program.url).on('complete', function(data){
+	    fs.writeFile('./ilias.html', data, function(err, data1) {
+      });
+	    var checkJson = checkHtmlFile('./ilias.html', program.checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	});
+    }
+    else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    } 
+}
+else {
     exports.checkHtmlFile = checkHtmlFile;
 }
